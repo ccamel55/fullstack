793d74c53921ffb28bd5bda315796127293821d8
Joshua <97867490+ccamel55@users.noreply.github.com>
2022-09-09 14:29:47 +1200

    calculator now has functionality, just need to sync it to the api

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -21,6 +21,9 @@ function App() {
   const [usePopup, setUsePopup] = useState(true);
   const [userName, setUserName] = useState(null);
 
+  const [calc, setCalc] = useState("");
+  const [wasCalc, setWasCalc] = useState(false);
+
   const USERNAME_TEXT_ID = "username_text_id";
 
   const updateTextData = (event:any) => {
@@ -35,6 +38,33 @@ function App() {
       setUsePopup(false);
   }
 
+  const updateCalc = (value:any) => {
+
+    const operators = ["+", "-", "*", "/"]
+
+    // only allow one operator
+    if ((operators.includes(value) && calc === "") || 
+        (operators.includes(value) && operators.includes(calc.slice(-1)))) {
+
+          // update the operator
+          setCalc(calc.substring(0, calc.length -1) + value);
+          return;
+        }
+    
+    // if we are working on a result only allow operators
+    if (wasCalc && !operators.includes(value))
+      return;
+
+    // work on result
+    setCalc(calc + value);
+    setWasCalc(false);
+  }
+
+  const updateResult = () => {
+    setWasCalc(true);
+    setCalc(eval(calc).toString());
+  }
+
   return (
     <div className="App">
       <Popup open={usePopup}>
@@ -45,41 +75,30 @@ function App() {
 
       <HistoryWindow>
         <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
-        <History data={"some calc"}/>
       </HistoryWindow>
 
       <Window>
-        <Text str="Output"/>
+        <Text str= { calc }/>
         <ButtonGrid>
-          <Button name='+' callback={() => {}}/>
-          <Button name='-' callback={() => {}}/>
-          <Button name='x' callback={() => {}}/>
-          <Button name='/' callback={() => {}}/>
+          <Button name='+' callback={() => { updateCalc("+"); }}/>
+          <Button name='-' callback={() => { updateCalc("-"); }}/>
+          <Button name='x' callback={() => { updateCalc("*"); }}/>
+          <Button name='/' callback={() => { updateCalc("/"); }}/>
 
-          <Button name='1' callback={() => {}}/>
-          <Button name='2' callback={() => {}}/>
-          <Button name='3' callback={() => {}}/>
-          <Button name='4' callback={() => {}}/>
+          <Button name='1' callback={() => { updateCalc("1"); }}/>
+          <Button name='2' callback={() => { updateCalc("2"); }}/>
+          <Button name='3' callback={() => { updateCalc("3"); }}/>
+          <Button name='4' callback={() => { updateCalc("4"); }}/>
 
-          <Button name='5' callback={() => {}}/>
-          <Button name='6' callback={() => {}}/>     
-          <Button name='7' callback={() => {}}/>
-          <Button name='8' callback={() => {}}/>
+          <Button name='5' callback={() => { updateCalc("5"); }}/>
+          <Button name='6' callback={() => { updateCalc("6"); }}/>     
+          <Button name='7' callback={() => { updateCalc("7"); }}/>
+          <Button name='8' callback={() => { updateCalc("8"); }}/>
 
-          <Button name='9' callback={() => {}}/>
-          <Button name='0' callback={() => {}}/>
-          <Button name='C' callback={() => {}}/>
-          <Button name='=' callback={() => {}}/>
+          <Button name='9' callback={() => { updateCalc("9") }}/>
+          <Button name='0' callback={() => { updateCalc("0") }}/>
+          <Button name='C' callback={() => { setCalc(""); setWasCalc(false); }}/>
+          <Button name='=' callback={() => { updateResult(); }}/>
         </ButtonGrid>
       </Window>
     </div>
